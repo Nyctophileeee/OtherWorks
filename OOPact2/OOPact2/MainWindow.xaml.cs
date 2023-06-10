b6ea76a2e554af93036ebeb636124ea17504853b
Lester <102751769+Nyctophileeee@users.noreply.github.com>
2023-06-10 08:09:46 +0800

    Add files via upload

---
+++ OOPact2/OOPact2/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OOPact2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void accountButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            Window1 win = new Window1();
+            win.Show();
+        }
+
+        private void loginButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            Window2 win = new Window2();
+            win.Show();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+    }
+}
